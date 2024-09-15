3e42cade00c8a96172d649ca538754d57c4c3f17
mokwathedeveloper <moffatmokwa12@gmail.com>
2024-09-15 09:33:13 +0300

    feat: add string concatenation and template literals
    - Implemented string concatenation using both comma () and  operator.
    - Utilized ES6 template literals to create dynamic strings with variables for name and age.
    - Added logs to demonstrate string concatenation and template literals in action.

---
+++ basics/Concate_and_Template_String/ConcateTemplateString.js
@@ -0,0 +1,14 @@
+// concatinate string
+
+let str1= 'My course is'
+let str2 = 'JavaScript'
+
+console.log(str1,str2)
+console.log(str1+str2)
+
+// Template String (Es6)
+
+let firstName = "Mokwa";
+let Age = 37
+let aboutMe = `My name is ${firstName} and am ${Age}  old`
+console.log(aboutMe)
