a8be867043b216f3f3447dacb65c6bce8dc4e849
mokwathedeveloper <moffatmokwa12@gmail.com>
2024-09-15 14:34:26 +0300

    feat: demonstrate usage of typeof operator and data type conversions
    - Added examples for detecting data types using the  operator (string, number).
    - Implemented number-to-string conversion using concatenation and  method.
    - Showcased string-to-number conversion using unary plus () operator and  function.
    - Logged outputs to display the type and value of variables before and after conversion.

---
+++ basics/type_of _Keyword/type_of_ Keyword.js
@@ -0,0 +1,31 @@
+//typeof operator
+
+//datatypes (primitives)
+
+
+// string 
+// number
+//boolean
+//null
+//bigInt
+//symbols
+
+let str1 = "JavaScript"
+console.log(typeof str1,str1)
+
+let age = 22
+console.log(typeof age, age)
+
+//converting number to string
+
+let num1 = "40" 
+console.log(typeof num1, num1);
+let num01 = num1 + ""
+console.log(typeof num01, num01)
+// converting string to number
+let num2 = +num1
+console.log(typeof num2 , num2)
+// other method of converting number to string
+ let num3 = Number(num1)
+ console.log(typeof num3, num3)
+
