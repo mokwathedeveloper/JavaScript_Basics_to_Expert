167f29fc83c61f475774a57224ab5f63b1b2472c
mokwathedeveloper <moffatmokwa12@gmail.com>
2024-09-17 17:59:22 +0300

    feat: Implement basic if-else conditional statements in JavaScript
    - Added an if-else statement to check voter eligibility based on age (e.g., age greater than 18 is eligible).
    - Implemented an if-else logic to determine if a number is even or odd using the modulo operator.
    
    Conditional statements are fundamental to decision-making in JavaScript! 🗳️🔢

---
+++ basics/If_else_Conditional_Statement/If_else_Conditional_Statement.js
@@ -0,0 +1,21 @@
+// if else conditional statement
+
+
+let age = 18;
+
+if (age>18){
+    console.log("eligible to vote")
+}
+else {
+    console.log("Not eligible to vote")
+}
+
+
+let num1 = 10 ;
+
+if (num1%2 ===0){
+    console.log("Number is EVEN Number")
+}
+else {
+    console.log("Number is ODD Number")
+}
